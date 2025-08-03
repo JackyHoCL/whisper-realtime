b63620b72a7161ef19006eee88a0335480e249a5
Jacky <hocheuklung@hotmail.com>
2025-08-03 21:09:38 +0800

    update

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ VLLM_PATH = '/v1'
 VLLM_KEY = ''             #Your vllm key
 VLLM_PROTOCOL = 'http'
 VLLM_MODEL_NAME = 'whisper-3'  #Should be the same name as the served-model-name or model name in runModel.sh
-VLLM_DEFAULT_LANG = ''
+VLLM_DEFAULT_LANG = 'zh'
 
 #VAD
 VAD_THRESHOLD = 0.5               #The threshold is used to determine whether it is a speech activity in a single hop
 VAD_HOP_SIZE = 256
-VAD_SILENT_THRESHOLD = 15         #How may frames should wait before send to whisper
-VAD_SEND_THRESHOLD = 1000000      #Send if audio data accumulated number exceed threshold, 1000000 is around 5 seconds
-VAD_AVG_SCORE_THRESHOLD = 0.2     #The threshold is used to determine whether it is a speech activity in a single speech frame by calculating the avg result return by VAD_THRESHOLD
+VAD_SILENT_THRESHOLD = 3         #How may frames should wait before send to whisper
+VAD_SEND_THRESHOLD = 2000000      #Send if audio data accumulated number exceed threshold, 1000000 is around 5 seconds
+VAD_AVG_SCORE_THRESHOLD = 0.1     #The threshold is used to determine whether it is a speech activity in a single speech frame by calculating the avg result return by VAD_THRESHOLD
                                   #eg. 100 hops in a frame, 90 of them are 1, 10 of them are 0, the avg_score should be (90+0)/100 = 0.9
                                   #The recording is start when exceed the score.
-VAD_MIN_THRESHOLD = 300000        #Minimum length to transcribe
+VAD_MIN_THRESHOLD = 50000        #Minimum length to transcribe
 
 #SERVER
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 10928
-SERVER_RELOAD = False             #For debug and dev purpose, suggest False in production environment
+SERVER_RELOAD = True             #For debug and dev purpose, suggest False in production environment
 
 #AUDIO
-AUDIO_SAMPLE_RATE = 48000            #Input audio sampling rate
+AUDIO_SAMPLE_RATE = 16000            #Input audio sampling rate
 
 #HALLUCINATION
 HALLUCINATION_COLLECTOR = False     #Turn on hallucination collector to generate output.json? 
